1c40903d5d01eb8188b6f51fc97f73d6331a434f
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2023-04-28 16:24:03 +0100

    102. fix

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -6,10 +6,9 @@
  * counting_sort - sorts an array of integers in ascending order
  * using the Counting sort algorithm
  *
- * @count_arr: an array that will be used to keep track of the count of each
- * element in the input array
- * sorted_arr: an array that will hold the sorted output
- * ma_val: integer that holds the maximum value in the input array
+ * @car: an array that will be used to keep track of the counted array
+ * sar: an array that will hold the sorted array
+ * mval: integer that holds the maximum value in the input array
  * @array: array of integers to be sorted
  * @size: size of the array
  *
@@ -18,47 +17,47 @@
 
 void counting_sort(int *array, size_t size)
 {
-int i, j, max_val;
-int *count_arr = NULL, *sorted_arr = NULL;
+int i, j, mval;
+int *car = NULL, *sar = NULL;
 
 if (!array || size < 2)
 return;
 
-max_val = array[0];
+mval = array[0];
 for (i = 1; i < (int)size; i++)
 {
-if (array[i] > max_val)
-max_val = array[i];
+if (array[i] > mval)
+mval = array[i];
 }
 
-count_arr = malloc(sizeof(int) * (max_val + 1));
-sorted_arr = malloc(sizeof(int) * size);
-if (!count_arr || !sorted_arr)
+car = malloc(sizeof(int) * (mval + 1));
+sar = malloc(sizeof(int) * size);
+if (!car || !sar)
 return;
 
-for (i = 0; i < (max_val + 1); i++)
-count_arr[i] = 0;
+for (i = 0; i < (mval + 1); i++)
+car[i] = 0;
 
 for (i = 0; i < (int)size; i++)
-count_arr[array[i]]++;
+car[array[i]]++;
 
-for (i = 1; i < (max_val + 1); i++)
-count_arr[i] += count_arr[i - 1];
+for (i = 1; i < (m_val + 1); i++)
+car[i] += car[i - 1];
 
 for (i = (int)(size - 1); i >= 0; i--)
 {
-sorted_arr[count_arr[array[i]] - 1] = array[i];
-count_arr[array[i]]--;
+sar[car[array[i]] - 1] = array[i];
+car[array[i]]--;
 }
 
 for (i = 0; i < (int)size; i++)
-array[i] = sorted_arr[i];
+array[i] = sar[i];
 
 printf("Counting array:\n");
-for (j = 0; j < (max_val + 1); j++)
-printf("%d, ", count_arr[j]);
+for (j = 0; j < (mval + 1); j++)
+printf("%d, ", car[j]);
 printf("\n");
 
-free(count_arr);
-free(sorted_arr);
+free(car);
+free(sar);
 }
