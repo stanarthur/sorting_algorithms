20b59504e956766ce4786d361037b1afd8b033c9
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2023-04-28 16:49:17 +0100

    102. fixed

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -3,61 +3,56 @@
 #include <stdlib.h>
 
 /**
- * counting_sort - sorts an array of integers in ascending order
- * using the Counting sort algorithm
+ * counting_sort - Sorts an array of integers in ascending order using
+ * the Counting sort algorithm
  *
- * @car: an array that will be used to keep track of the counted array
- * sar: an array that will hold the sorted array
- * mval: integer that holds the maximum value in the input array
- * @array: array of integers to be sorted
- * @size: size of the array
- *
+ * @buff: n array that will be used to keep track of the count of each
+ * element in the input array
+ * @a: an array that will hold the sorted output
+ * @array: The array to sort.
+ * @size: The size of the array.
  * Return: no return
  */
-
 void counting_sort(int *array, size_t size)
 {
-int i, j, mval;
-int *car = NULL, *sar = NULL;
+	int n, i;
+	int *buff, *a;
 
-if (!array || size < 2)
-return;
+	if (size < 2)
+		return;
 
-mval = array[0];
-for (i = 1; i < (int)size; i++)
-{
-if (array[i] > mval)
-mval = array[i];
-}
+	for (n = i = 0; i < (int)size; i++)
+		if (array[i] > n)
+			n = array[i];
 
-car = malloc(sizeof(int) * (mval + 1));
-sar = malloc(sizeof(int) * size);
-if (!car || !sar)
-return;
+	buff = malloc(sizeof(int) * (n + 1));
+	if (!buff)
+		return;
 
-for (i = 0; i < (mval + 1); i++)
-car[i] = 0;
+	for (i = 0; i <= n; i++)
+		buff[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		buff[array[i]] += 1;
+	for (i = 1; i <= n; i++)
+		buff[i] += buff[i - 1];
 
-for (i = 0; i < (int)size; i++)
-car[array[i]]++;
+	print_array(buff, (n + 1));
+	a = malloc(sizeof(int) * (size + 1));
 
-for (i = 1; i < (m_val + 1); i++)
-car[i] += car[i - 1];
+	if (!a)
+	{
+		free(buff);
+		return;
+	}
+	for (i = 0; i < (int)size; i++)
+	{
+		a[buff[array[i]] - 1] = array[i];
+		buff[array[i]] -= 1;
+	}
 
-for (i = (int)(size - 1); i >= 0; i--)
-{
-sar[car[array[i]] - 1] = array[i];
-car[array[i]]--;
-}
+	for (i = 0; i < (int)size; i++)
+		array[i] = a[i];
 
-for (i = 0; i < (int)size; i++)
-array[i] = sar[i];
-
-printf("Counting array:\n");
-for (j = 0; j < (mval + 1); j++)
-printf("%d, ", car[j]);
-printf("\n");
-
-free(car);
-free(sar);
+	free(buff);
+	free(a);
 }
