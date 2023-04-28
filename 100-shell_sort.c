e23f4c42f7892556af9ca2933098fd2cc043a8ac
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2023-04-28 07:10:58 -0700

    100. fix

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,33 +12,32 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t i, j, space;
-    int tmp;
+size_t i, j, space, int tmp;
 
-    if (!array || size < 2)
-        return;
+if (!array || size < 2)
+return;
 
-    space = 1;
-    while (space < size / 3)
-        space = space * 3 + 1;
+space = 1;
+while (space < size / 3)
+space = space * 3 + 1;
 
-    while (space > 0)
-    {
-        for (i = space; i < size; i++)
-        {
-            tmp = array[i];
-            j = i;
+while (space > 0)
+{
+for (i = space; i < size; i++)
+{
+tmp = array[i];
+j = i;
 
-            while (j >= space && array[j - space] > tmp)
-            {
-                array[j] = array[j - space];
-                j -= space;
-            }
+while (j >= space && array[j - space] > tmp)
+{
+array[j] = array[j - space];
+j -= space;
+}
 
-            array[j] = tmp;
-        }
+array[j] = tmp;
+}
 
-        space = (space - 1) / 3;
-        print_array(array, size);
-    }
+space = (space - 1) / 3;
+print_array(array, size);
+}
 }
